45b561f0f3381252f16919b0f7a29aae0830f537
christian.kirchhof <christian.kirchhof@rwth-aachen.de>
2020-11-22 01:49:01 +0100

    Fix code generation for "?" port access

--- generators/montithings2cpp/src/main/java/montithings/generator/visitor/CppMontiThingsPrettyPrinter.java
+++ generators/montithings2cpp/src/main/java/montithings/generator/visitor/CppMontiThingsPrettyPrinter.java
@@ -3,26 +3,189 @@ package montithings.generator.visitor;
 
 import arcbasis._symboltable.ComponentTypeSymbol;
 import arcbasis._symboltable.PortSymbol;
+import de.monticore.expressions.expressionsbasis._ast.ASTExpression;
 import de.monticore.expressions.expressionsbasis._ast.ASTNameExpression;
+import de.monticore.expressions.setexpressions._ast.*;
+import de.monticore.expressions.setexpressions._visitor.SetExpressionsVisitor;
 import de.monticore.prettyprint.CommentPrettyPrinter;
-import de.se_rwth.commons.logging.Log;
+import de.monticore.prettyprint.IndentPrinter;
 import montiarc._symboltable.IMontiArcScope;
 import montithings._ast.ASTIsPresentExpression;
 import montithings._visitor.MontiThingsPrettyPrinter;
+import montithings._visitor.MontiThingsVisitor;
 import montithings.generator.helper.ComponentHelper;
+import net.sourceforge.plantuml.Log;
 import portextensions._ast.ASTSyncStatement;
+import setdefinitions._ast.ASTSetDefinition;
+import setdefinitions._ast.ASTSetValueList;
+import setdefinitions._ast.ASTSetValueRange;
+import setdefinitions._ast.ASTSetValueRegEx;
+import setdefinitions._visitor.SetDefinitionsVisitor;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Stack;
 import java.util.stream.Collectors;
 
 import static montithings.generator.visitor.CppPrettyPrinterUtils.capitalize;
 import static montithings.generator.visitor.CppPrettyPrinterUtils.isSet;
 
 /**
- * Handles MontiThings Statements
+ * TODO
+ *
+ * @since 23.07.20
  */
 public class CppMontiThingsPrettyPrinter extends MontiThingsPrettyPrinter {
+  private MontiThingsVisitor realThis;
+
+  public CppMontiThingsPrettyPrinter(IndentPrinter printer) {
+    super(printer);
+    this.realThis = this;
+  }
+
+  Stack<ASTExpression> expressions = new Stack<>();
+
+  @Override
+  public void handle(ASTIsInExpression node) {
+    CommentPrettyPrinter.printPreComments(node, getPrinter());
+    expressions.push(node.getElem());
+    node.getSet().accept(getRealThis());
+    expressions.pop();
+    CommentPrettyPrinter.printPostComments(node, getPrinter());
+  }
+
+  @Override
+  public void handle(montithings._ast.ASTSetInExpression node) {
+    CommentPrettyPrinter.printPreComments(node, getPrinter());
+    expressions.push(node.getElem());
+    node.getSet().accept(getRealThis());
+    expressions.pop();
+    CommentPrettyPrinter.printPostComments(node, getPrinter());
+  }
+
+  @Override
+  public void handle(ASTSetInExpression node) {
+    CommentPrettyPrinter.printPreComments(node, getPrinter());
+    expressions.push(node.getElem());
+    node.getSet().accept(getRealThis());
+    expressions.pop();
+    CommentPrettyPrinter.printPostComments(node, getPrinter());
+  }
+
+  @Override
+  public void handle(ASTUnionExpressionInfix node) {
+    CommentPrettyPrinter.printPreComments(node, getPrinter());
+    getPrinter().print("(");
+    node.getLeft().accept(getRealThis());
+    getPrinter().print(")");
+    getPrinter().print(" || ");
+    getPrinter().print("(");
+    node.getRight().accept(getRealThis());
+    getPrinter().print(")");
+    CommentPrettyPrinter.printPostComments(node, getPrinter());
+  }
+
+  @Override
+  public void handle(ASTIntersectionExpressionInfix node) {
+    CommentPrettyPrinter.printPreComments(node, getPrinter());
+    getPrinter().print("(");
+    node.getLeft().accept(getRealThis());
+    getPrinter().print(")");
+    getPrinter().print(" && ");
+    getPrinter().print("(");
+    node.getRight().accept(getRealThis());
+    getPrinter().print(")");
+    CommentPrettyPrinter.printPostComments(node, getPrinter());
+  }
+
+  @Override
+  public void handle(ASTSetAndExpression node) {
+    Log.error("0xMT820 SetAndExpression is not supported.");
+  }
+
+  @Override
+  public void handle(ASTSetOrExpression node) {
+    Log.error("0xMT821 SetOrExpression is not supported.");
+  }
+
+  @Override
+  public void handle(ASTUnionExpressionPrefix node) {
+    Log.error("0xMT822 UnionExpression is not supported.");
+  }
+
+  @Override
+  public void handle(ASTIntersectionExpressionPrefix node) {
+    Log.error("0xMT823 IntersectionExpression is not supported.");
+  }
+
+  @Override
+  public void handle(ASTSetValueList node){
+    for (int i = 0; i < node.sizeExpressions(); i++) {
+      getPrinter().print("(");
+      expressions.peek().accept(getRealThis());
+      getPrinter().print(" == ");
+      node.getExpression(i).accept(getRealThis());
+      getPrinter().print(")");
+
+      if (i < node.sizeExpressions()-1) {
+        getPrinter().print(" || ");
+      }
+    }
+  }
+
+  @Override
+  public void handle(ASTSetValueRange node){
+    getPrinter().print("(");
+		expressions.peek().accept(getRealThis());
+    getPrinter().print(" >= ");
+    node.getLowerBound().accept(getRealThis());
+    getPrinter().print(" && ");
+
+		if (node.isPresentStepsize()) {
+      getPrinter().print("((");
+      expressions.peek().accept(getRealThis());
+      getPrinter().print(" - ");
+      node.getLowerBound().accept(getRealThis());
+      getPrinter().print(")");
+      getPrinter().print(" % ");
+      node.getStepsize().accept(getRealThis());
+      getPrinter().print(" == 0 ");
+      getPrinter().print(") && ");
+    }
+    expressions.peek().accept(getRealThis());
+    getPrinter().print(" <= ");
+    node.getUpperBound().accept(getRealThis());
+    getPrinter().print(")");
+  }
+
+  @Override
+  public void visit(ASTSetValueRegEx node){
+    getPrinter().print("(");
+    getPrinter().print("std::regex_match(");
+    getPrinter().print("((std::ostringstream&)(std::ostringstream(\"\") << ");
+    expressions.peek().accept(getRealThis());
+    getPrinter().print(")).str(), ");
+
+    getPrinter().print("std::regex(");
+    node.getFormat().accept(getRealThis());
+    getPrinter().print(")))");
+  }
+
+  @Override public void traverse(ASTSetValueRegEx node) {
+    // do not visit format node
+  }
+
+  @Override
+  public void handle(ASTSetDefinition node){
+    for (int i = 0; i < node.sizeSetAllowedValuess(); i++) {
+      getPrinter().print("(");
+      node.getSetAllowedValues(i).accept(getRealThis());
+      getPrinter().print(")");
+      if (i < node.sizeSetAllowedValuess()-1) {
+        getPrinter().print(" || ");
+      }
+    }
+  }
 
   @Override
   public void handle(ASTIsPresentExpression node) {
@@ -39,7 +202,7 @@ public class CppMontiThingsPrettyPrinter extends MontiThingsPrettyPrinter {
       comp = (ComponentTypeSymbol) s.getSpanningSymbol();
     }
     else {
-      Log.error("ASTNameExpression " + node.getNameExpression().getName()
+      de.se_rwth.commons.logging.Log.error("ASTNameExpression " + node.getNameExpression().getName()
         + "has an unknown scope (neither statement nor automaton)");
       // throw useless exception to make compiler happy with accessing "comp" afterwards
       throw new IllegalArgumentException();
@@ -88,5 +251,24 @@ public class CppMontiThingsPrettyPrinter extends MontiThingsPrettyPrinter {
     IMontiArcScope s = (IMontiArcScope) node.getEnclosingScope();
     String name = node.getName();
     return s.resolvePort(name);
+  }
+
+  @Override public MontiThingsVisitor getRealThis() {
+    return realThis;
+  }
+
+  @Override
+  public void setRealThis(MontiThingsVisitor realThis) {
+    this.realThis = realThis;
+  }
+
+  @Override
+  public void setRealThis(SetDefinitionsVisitor realThis) {
+    this.realThis = (MontiThingsVisitor) realThis;
+  }
+
+  @Override
+  public void setRealThis(SetExpressionsVisitor realThis) {
+    this.realThis = (MontiThingsVisitor) realThis;
   }
 }
--- languages/montithings/src/main/java/montithings/_visitor/MontiThingsPrettyPrinter.java
+++ languages/montithings/src/main/java/montithings/_visitor/MontiThingsPrettyPrinter.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 public class MontiThingsPrettyPrinter implements MontiThingsVisitor {
 
-  private MontiThingsVisitor realThis = this;
+  protected MontiThingsVisitor realThis = this;
   protected IndentPrinter printer;
 
   public MontiThingsPrettyPrinter() {
