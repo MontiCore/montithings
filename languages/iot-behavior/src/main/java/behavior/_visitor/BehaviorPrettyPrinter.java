f4086d056106433f638128f6bf36b108852564ef
Anno Kleiss <anno.kleiss@rwth-aachen.de>
2021-02-05 15:07:02 +0100

    use SIUnitLiterals

--- languages/iot-behavior/src/main/java/behavior/_visitor/BehaviorPrettyPrinter.java
+++ languages/iot-behavior/src/main/java/behavior/_visitor/BehaviorPrettyPrinter.java
@@ -1,15 +1,10 @@
 package behavior._visitor;
 
 import behavior._ast.ASTAfterStatement;
-import behavior._ast.ASTBehaviorNode;
 import behavior._ast.ASTEveryStatement;
 import com.google.common.base.Preconditions;
-import de.monticore.expressions.expressionsbasis._ast.ASTExpressionsBasisNode;
 import de.monticore.prettyprint.IndentPrinter;
 import org.codehaus.commons.nullanalysis.NotNull;
-
-import java.util.Iterator;
-import java.util.List;
 
 public class BehaviorPrettyPrinter implements BehaviorVisitor {
 
@@ -41,58 +36,18 @@ public class BehaviorPrettyPrinter implements BehaviorVisitor {
     return this.printer;
   }
 
-  public <T extends ASTExpressionsBasisNode> void acceptSeperatedList(@NotNull List<T> list){
-    if (list.size() <= 0) {
-      return;
-    }
-    Iterator<T> iterator = list.iterator();
-    iterator.next().accept(this.getRealThis());
-    while (iterator.hasNext()) {
-      this.getPrinter().print(", ");
-      iterator.next().accept(this.getRealThis());
-    }
-  }
-
-  public <T extends ASTBehaviorNode> void acceptSeperatedSetList(@NotNull List<T> list){
-    if (list.size() <= 0) {
-      return;
-    }
-    Iterator<T> iterator = list.iterator();
-    iterator.next().accept(this.getRealThis());
-    while (iterator.hasNext()) {
-      this.getPrinter().print(", ");
-      iterator.next().accept(this.getRealThis());
-    }
-  }
-
   @Override
   public void handle (ASTAfterStatement node){
     getPrinter().print("after ");
-    node.getNatLiteral().accept(getRealThis());
-    getPrinter().print(" ");
-    if(node.isPresentSeconds()){
-      getPrinter().print("sec");
-    } else if (node.isPresentMilliseconds()){
-      getPrinter().print("msec");
-    } else {
-      getPrinter().print("min");
-    }
+    node.getSIUnitLiteral().accept(getRealThis());
     getPrinter().print(" ");
     node.getMCJavaBlock().accept(getRealThis());
   }
 
   @Override
   public void handle (ASTEveryStatement node){
-    getPrinter().print("after ");
-    node.getNatLiteral().accept(getRealThis());
-    getPrinter().print(" ");
-    if(node.isPresentSeconds()){
-      getPrinter().print("sec");
-    } else if (node.isPresentMilliseconds()){
-      getPrinter().print("msec");
-    } else {
-      getPrinter().print("min");
-    }
+    getPrinter().print("every ");
+    node.getSIUnitLiteral().accept(getRealThis());
     getPrinter().print(" ");
     node.getMCJavaBlock().accept(getRealThis());
   }
