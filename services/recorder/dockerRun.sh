bdb9901a81121710f385e9811791e89016fc8b1e
Lukas Malcher <lukas.malcher@rwth-aachen.de>
2021-04-01 01:07:16 +0200

    Add directory mount to allow access to recorded file

--- services/recorder/dockerRun.sh
+++ services/recorder/dockerRun.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm --net=host monithings.recorder -i localhost:12345
+docker run --rm --net=host -v ${PWD}/docker_recordings:/usr/src/app/services/recorder/recordings monithings.recorder -i localhost:12345
