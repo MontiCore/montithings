7184ef2d2580bc3c4f2b2dceed41ed943be7bb94
christian.kirchhof <christian.kirchhof@rwth-aachen.de>
2021-04-11 19:28:16 +0200

    Remove deprecated functions in MontiThingsTool

--- languages/montithings/src/main/java/montithings/MontiThingsTool.java
+++ languages/montithings/src/main/java/montithings/MontiThingsTool.java
@@ -1,11 +1,7 @@
 /* (c) https://github.com/MontiCore/monticore */
 package montithings;
 
-import arcbasis._ast.ASTArcBasisNode;
-import arcbasis._ast.ASTComponentType;
-import arcbasis._symboltable.ComponentTypeSymbol;
 import com.google.common.base.Preconditions;
-import com.google.common.collect.Sets;
 import de.monticore.cd4code.CD4CodeMill;
 import de.monticore.cd4code._cocos.CD4CodeCoCoChecker;
 import de.monticore.cd4code._parser.CD4CodeParser;
@@ -28,15 +24,19 @@ import montiarc._ast.ASTMACompilationUnit;
 import montithings._auxiliary.BasicSymbolsMillForMontiThings;
 import montithings._cocos.MontiThingsCoCoChecker;
 import montithings._parser.MontiThingsParser;
-import montithings._symboltable.*;
+import montithings._symboltable.IMontiThingsArtifactScope;
+import montithings._symboltable.IMontiThingsGlobalScope;
+import montithings._symboltable.IMontiThingsScope;
+import montithings._symboltable.MontiThingsSymbolTableCreatorDelegator;
 import montithings.cocos.MontiThingsCoCos;
 import montithings.util.ParserUtil;
 import org.codehaus.commons.nullanalysis.NotNull;
 
-import java.io.File;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Optional;
 
 public class MontiThingsTool {
 
@@ -155,11 +155,13 @@ public class MontiThingsTool {
   }
 
   public Collection<ASTMACompilationUnit> parseModels(@NotNull IMontiThingsGlobalScope scope) {
-    return (Collection<ASTMACompilationUnit>) ParserUtil.parseModels(scope, MT_FILE_EXTENSION, new MontiThingsParser());
+    return (Collection<ASTMACompilationUnit>) ParserUtil
+      .parseModels(scope, MT_FILE_EXTENSION, new MontiThingsParser());
   }
 
   public Collection<ASTCDCompilationUnit> parseModels(@NotNull ICD4CodeGlobalScope scope) {
-    return (Collection<ASTCDCompilationUnit>) ParserUtil.parseModels(scope, CD_FILE_EXTENSION, new CD4CodeParser());
+    return (Collection<ASTCDCompilationUnit>) ParserUtil
+      .parseModels(scope, CD_FILE_EXTENSION, new CD4CodeParser());
   }
 
   public Optional<ASTMACompilationUnit> parseMT(@NotNull String filename) {
@@ -171,112 +173,13 @@ public class MontiThingsTool {
   }
 
   public Collection<ASTMACompilationUnit> parseMT(@NotNull Path path) {
-    return (Collection<ASTMACompilationUnit>) ParserUtil.parse(path, MT_FILE_EXTENSION, new MontiThingsParser());
+    return (Collection<ASTMACompilationUnit>) ParserUtil
+      .parse(path, MT_FILE_EXTENSION, new MontiThingsParser());
   }
 
   public Collection<ASTCDCompilationUnit> parseCD(@NotNull Path path) {
-    return (Collection<ASTCDCompilationUnit>) ParserUtil.parse(path, CD_FILE_EXTENSION, new CD4CodeParser());
-  }
-
-  @Deprecated
-  public boolean checkCoCos(@NotNull ASTArcBasisNode node) {
-    Preconditions.checkArgument(node != null);
-    Preconditions.checkState(this.isSymTabInitialized, "Please initialize symbol-table before "
-      + "checking cocos.");
-    this.getMTChecker().checkAll(node);
-    if (Log.getErrorCount() != 0) {
-      Log.debug("Found " + Log.getErrorCount() + " errors in node " + node + ".", "XX");
-      return false;
-    }
-    return true;
-  }
-
-  /**
-   * Loads a ComponentSymbol with the passed componentName. The componentName is the full qualified
-   * name of the component model. Modelpaths are folders relative to the project path and containing
-   * the packages the models are located in. When the ComponentSymbol is resolvable it is returned.
-   * Otherwise the optional is empty.
-   *
-   * @param componentName Name of the component
-   * @param modelPaths    Folders containing the packages with models
-   * @return an {@code Optional} of the loaded component type
-   * @deprecated
-   */
-  @Deprecated
-  public Optional<ComponentTypeSymbol> loadComponentSymbolWithoutCocos(String componentName,
-    File... modelPaths) {
-    IMontiThingsScope s = initSymbolTable(modelPaths);
-    return s.resolveComponentType(componentName);
-  }
-
-  @Deprecated
-  public Optional<ComponentTypeSymbol> loadComponentSymbolWithCocos(String componentName,
-    File... modelPaths) {
-    Optional<ComponentTypeSymbol> compSym = loadComponentSymbolWithoutCocos(componentName,
-      modelPaths);
-
-    compSym.ifPresent(componentTypeSymbol -> this.checkCoCos(componentTypeSymbol.getAstNode()));
-
-    return compSym;
-  }
-
-  /**
-   * Loads the AST of the passed model with name componentName. The componentName is the fully
-   * qualified. Modelpaths are folders relative to the project path and containing the packages the
-   * models are located in. When the ComponentSymbol is resolvable it is returned. Otherwise the
-   * optional is empty.
-   *
-   * @param modelPath The model path containing the package with the model
-   * @param model     the fully qualified model name
-   * @return the AST node of the model
-   * @deprecated
-   */
-  @Deprecated
-  public Optional<ASTComponentType> getAstNode(String modelPath, String model) {
-    // ensure an empty log
-    Log.getFindings().clear();
-    Optional<ComponentTypeSymbol> comp = loadComponentSymbolWithoutCocos(model,
-      Paths.get(modelPath).toFile());
-
-    if (!comp.isPresent()) {
-      Log.error("Model could not be resolved!");
-      return Optional.empty();
-    }
-
-    if (!comp.get().isPresentAstNode()) {
-      Log.debug("Symbol not linked with node.", "XX");
-      return Optional.empty();
-    }
-    return Optional.of(comp.get().getAstNode());
-  }
-
-  /**
-   * Initializes the Symboltable by introducing scopes for the passed modelpaths. It does not create
-   * the symbol table! Symbols for models within the modelpaths are not added to the symboltable
-   * until resolve() is called. Modelpaths are relative to the project path and do contain all the
-   * packages the models are located in. E.g. if model with fqn a.b.C lies in folder
-   * src/main/resources/models/a/b/C.arc, the modelpath is src/main/resources.
-   *
-   * @param modelPaths paths of all folders containing models
-   * @return The initialized symbol table
-   * @deprecated
-   */
-  @Deprecated
-  public IMontiThingsScope initSymbolTable(File... modelPaths) {
-    Set<Path> p = Sets.newHashSet();
-    for (File mP : modelPaths) {
-      p.add(Paths.get(mP.getAbsolutePath()));
-    }
-    final ModelPath mp = new ModelPath(p);
-    IMontiThingsGlobalScope montiThingsGlobalScope = MontiThingsMill.montiThingsGlobalScopeBuilder()
-      .setModelPath(mp).setModelFileExtension(MT_FILE_EXTENSION).build();
-    ICD4CodeGlobalScope cd4CodeGlobalScope = CD4CodeMill.cD4CodeGlobalScopeBuilder()
-      .setModelPath(mp).setModelFileExtension(CD_FILE_EXTENSION).build();
-    this.resolvingDelegates(montiThingsGlobalScope, cd4CodeGlobalScope);
-    this.addBasicTypes(montiThingsGlobalScope);
-    this.addLibraryFunctions(montiThingsGlobalScope);
-    isSymTabInitialized = true;
-    return montiThingsGlobalScope;
+    return (Collection<ASTCDCompilationUnit>) ParserUtil
+      .parse(path, CD_FILE_EXTENSION, new CD4CodeParser());
   }
 
   public void addBasicTypes(@NotNull IMontiThingsScope scope) {
@@ -347,21 +250,5 @@ public class MontiThingsTool {
         .build();
     now_ns.setSpannedScope(nowParameters);
     scope.add(now_ns);
-  }
-
-  /**
-   * Initializes the Symboltable by introducing scopes for the passed modelpaths. It does not create
-   * the symbol table! Symbols for models within the modelpaths are not added to the symboltable
-   * until resolve() is called. Modelpaths are relative to the project path and do contain all the
-   * packages the models are located in. E.g. if model with fqn a.b.C lies in folder
-   * src/main/resources/models/a/b/C.arc, the modelPath is src/main/resources.
-   *
-   * @param modelPath The model path for the symbol table
-   * @return the initialized symbol table
-   * @deprecated
-   */
-  @Deprecated
-  public IMontiThingsScope initSymbolTable(String modelPath) {
-    return initSymbolTable(Paths.get(modelPath).toFile());
   }
 }
