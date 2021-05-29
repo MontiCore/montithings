9364785b79e6428fbdec635c5bf8e2bee1836080
Lukas Malcher <lukas.malcher@rwth-aachen.de>
2021-05-29 13:58:41 +0200

    Fix docker; split frontend and middleware containers

--- services/log-tracing/dockerRun.sh
+++ services/log-tracing/dockerRun.sh
@@ -3,4 +3,5 @@
 
 docker network ls | grep montithings > /dev/null || docker network create --driver bridge montithings
 
-docker run --rm -d --net=montithings --name logtracer_middleware -p 3000:3000  monithings.logtracer_middleware --DCPSConfigFile dcpsconfig.ini
+./frontend/dockerRun.sh
+docker run --rm -d --net=montithings --name logtracer_middleware -p 8080:8080 -p 3000:3000  monithings.logtracer_middleware --DCPSConfigFile dcpsconfig.ini
--- services/log-tracing/entrypoint.sh
+++ services/log-tracing/entrypoint.sh
@@ -2,8 +2,6 @@
 # (c) https://github.com/MontiCore/monticore
 . /usr/src/app/opendds/setenv.sh
 
-cd frontend
-yarn serve &
 
-cd ../build/logtracer_middleware/bin
+cd build/logtracer_middleware/bin
 ./logtracer_middleware "$@"
