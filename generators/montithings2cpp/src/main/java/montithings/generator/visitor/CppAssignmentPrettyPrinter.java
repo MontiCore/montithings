7368ff7f335b38de643045849a36ed17a890f588
Lukas Malcher <lukas.malcher@rwth-aachen.de>
2021-07-15 11:55:54 +0200

    Fix argument swap bug

--- generators/montithings2cpp/src/main/java/montithings/generator/visitor/CppAssignmentPrettyPrinter.java
+++ generators/montithings2cpp/src/main/java/montithings/generator/visitor/CppAssignmentPrettyPrinter.java
@@ -200,7 +200,7 @@ public class CppAssignmentPrettyPrinter extends AssignmentExpressionsPrettyPrint
       }
       getPrinter().println(" );");
 
-      if (isLogTracingEnabled) {
+      if (isLogTracingEnabled && suppressPostconditionCheck) {
         IMontiThingsScope componentScope = getScopeOfEnclosingComponent(node);
         ComponentTypeSymbol component = (ComponentTypeSymbol) componentScope.getSpanningSymbol();
         List<VariableSymbol> stateVariables = ComponentHelper.getVariablesAndParameters(component);
