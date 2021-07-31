4bd020f1a13e515fed59f141064d6037318dac2a
Philipp Schneider <12638-ps2000@users.noreply.git.rwth-aachen.de>
2021-07-31 17:07:51 +0200

    added convenience constructor for LocationSpecifier

--- src/main/java/ps/deployment/server/data/LocationSpecifier.java
+++ src/main/java/ps/deployment/server/data/LocationSpecifier.java
@@ -4,6 +4,16 @@ public class LocationSpecifier {
   
   private String building, floor, room;
   
+  public LocationSpecifier() {
+    
+  }
+  
+  public LocationSpecifier(String building, String floor, String room) {
+    this.building = building;
+    this.floor = floor;
+    this.room = room;
+  }
+  
   public String getBuilding() {
     return building;
   }
@@ -28,6 +38,11 @@ public class LocationSpecifier {
     this.room = room;
   }
   
+  /**@return whether this LocationSpecifier is fully specified, i.e. whether building, floor and room are all set.*/
+  public boolean isFullySpecified() {
+    return this.building != null && this.floor != null && this.room != null;
+  }
+  
   @Override
   public String toString() {
     return "LocationSpecifier [building=" + building + ", floor=" + floor + ", room=" + room + "]";
@@ -35,19 +50,22 @@ public class LocationSpecifier {
   
   /**
    * Constructs a String like "building1_floor2_room3"
+   * 
    * @return this String
-   * */
+   */
   public String toPrologString() {
     StringBuilder sb = new StringBuilder();
-    if(building != null && !building.isEmpty()) {
+    if (building != null && !building.isEmpty()) {
       sb.append("building").append(building);
     }
-    if(floor != null && !floor.isEmpty()) {
-      if(sb.length() > 0) sb.append('_');
+    if (floor != null && !floor.isEmpty()) {
+      if (sb.length() > 0)
+        sb.append('_');
       sb.append("floor").append(floor);
     }
-    if(room != null && !room.isEmpty()) {
-      if(sb.length() > 0) sb.append('_');
+    if (room != null && !room.isEmpty()) {
+      if (sb.length() > 0)
+        sb.append('_');
       sb.append("room").append(room);
     }
     return sb.toString();
@@ -59,6 +77,46 @@ public class LocationSpecifier {
     loc.setFloor(floor);
     loc.setRoom(room);
     return loc;
+  }
+  
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((building == null) ? 0 : building.hashCode());
+    result = prime * result + ((floor == null) ? 0 : floor.hashCode());
+    result = prime * result + ((room == null) ? 0 : room.hashCode());
+    return result;
+  }
+  
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    LocationSpecifier other = (LocationSpecifier) obj;
+    if (building == null) {
+      if (other.building != null)
+        return false;
+    }
+    else if (!building.equals(other.building))
+      return false;
+    if (floor == null) {
+      if (other.floor != null)
+        return false;
+    }
+    else if (!floor.equals(other.floor))
+      return false;
+    if (room == null) {
+      if (other.room != null)
+        return false;
+    }
+    else if (!room.equals(other.room))
+      return false;
+    return true;
   }
   
 }
