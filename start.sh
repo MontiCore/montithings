3ba11fca1dba33a2b226949cbdefa4ca645628c8
Philipp Schneider <12638-ps2000@users.noreply.git.rwth-aachen.de>
2021-08-10 01:13:00 +0200

    added start script

---
+++ start.sh
@@ -0,0 +1,2 @@
+# docker run --rm -p 5004:4567 registry.git.rwth-aachen.de/monticore/montithings/deployment/iot-manager/prolog-generator:master
+docker run --rm -p 5004:4567 registry.git.rwth-aachen.de/monticore/montithings/deployment/iot-manager/prolog-generator:patched
