0ffb078e4fbda3813eddf240aef0f14651801450
Philipp Schneider <12638-ps2000@users.noreply.git.rwth-aachen.de>
2021-08-23 19:49:31 +0200

    use __ instead of . for multi instancing

--- generators/montithings2cpp/src/main/resources/rte/montithings-RTE/Utils.cpp
+++ generators/montithings2cpp/src/main/resources/rte/montithings-RTE/Utils.cpp
@@ -39,16 +39,17 @@ getEnclosingComponentName (const std::string& input)
 }
 
 std::string getModelInstanceName(const std::string& instanceName) {
-  int lastDot = instanceName.find_last_of('.');
-  if (lastDot != std::string::npos && lastDot != instanceName.length() - 1) {
+  int lastUnderscore = instanceName.find_last_of('_');
+  if (lastUnderscore != std::string::npos && lastUnderscore != instanceName.length() - 1 && lastUnderscore > 0) {
     try {
-      // extract part behind last dot
-      std::string lastPart = instanceName.substr(lastDot + 1);
-      // try parsing last part as number
-      std::stoi(lastPart);
-      // return instanceName without the trailing dot (1) and lastPart
-      // (lastPart.length())
-      return instanceName.substr(0, instanceName.length() - lastPart.length() - 1);
+      // assert we have two underscores
+      if(instanceName.at(lastUnderscore - 1) == '_') {
+        // extract part behind last (double) underscore
+        std::string lastPart = instanceName.substr(lastUnderscore + 1);
+        // try parsing last part as number
+        std::stoi(lastPart);
+        return instanceName.substr(0, instanceName.length() - lastPart.length() - 2);
+      }
     } catch (std::logic_error e) {
       // last part is not a number
     }
