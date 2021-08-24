9bc2bc84d85100c48a483cefeb26d79a0d8f9dbd
Lukas Malcher <lukas.malcher@rwth-aachen.de>
2021-08-24 18:18:55 +0200

    Fix missing boost packages, fix reference to DDS sources even though they are not present anymore

--- services/log-tracing/frontend/dockerRun.sh
+++ services/log-tracing/frontend/dockerRun.sh
@@ -3,4 +3,4 @@
 
 docker network ls | grep montithings > /dev/null || docker network create --driver bridge montithings
 
-docker run --rm -d --net=montithings --name logtracer_frontend -p 3000:3000 logtracer_frontend --DCPSConfigFile dcpsconfig.ini
+docker run --rm -d --net=montithings --name logtracer_frontend -p 3000:3000 registry.git.rwth-aachen.de/monticore/montithings/core/logtracer_frontend --DCPSConfigFile dcpsconfig.ini
--- services/log-tracing/frontend/dockerRunLocal.sh
+++ services/log-tracing/frontend/dockerRunLocal.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # (c) https://github.com/MontiCore/monticore
 
-docker run --rm -d --net=host --name logtracer_frontend logtracer_frontend
+docker run --rm -d --net=host -p 3000:3000 --name logtracer_frontend registry.git.rwth-aachen.de/monticore/montithings/core/logtracer_frontend
