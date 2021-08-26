9115a2ea0696e9cf406f3e85dba8ae49a544bfbd
Lukas Malcher <lukas.malcher@rwth-aachen.de>
2021-08-26 09:40:03 +0200

    Add support mqtt host and port arguments

--- services/log-tracing/dockerRunLocal.sh
+++ services/log-tracing/dockerRunLocal.sh
@@ -2,4 +2,16 @@
 # (c) https://github.com/MontiCore/monticore
 
 ./frontend/dockerRunLocal.sh
-docker run --rm -d --net=host -p 8080:8080 --name logtracer_middleware  registry.git.rwth-aachen.de/monticore/montithings/core/logtracer_middleware --DCPSConfigFile dcpsconfig.ini "$@"
+
+# Unfortunately, the docker option --net=host is not supported by iOS and is ignored.
+# Therefore, we have to find out what os is used and set the MQTT ip accordingly
+# On Linux, localhost is used since the host network is used (this is done to allow multicast, see the DDS broker).
+# On iOS, the montithings network is used instead. Therefore, we have to pass the ip of the host machine.
+os="$(uname -s)"
+case "${os}" in
+    Linux*)     mqttip=127.0.0.1;;
+    Darwin*)    mqttip=host.docker.internal;;
+    *)          echo "unknown os!" && exit 1
+esac
+
+docker run --rm -d --net=host -p 8080:8080 --name logtracer_middleware  registry.git.rwth-aachen.de/monticore/montithings/core/logtracer_middleware --DCPSConfigFile dcpsconfig.ini --mqtt-broker-host ${mqttip} "$@"
