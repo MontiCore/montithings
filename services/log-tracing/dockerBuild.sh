5d87c86bdc06732b430f79cdb259e8105802f511
Lukas Malcher <lukas.malcher@rwth-aachen.de>
2021-09-01 11:16:54 +0200

    remove net=host option in case host is MacOS

--- services/log-tracing/dockerBuild.sh
+++ services/log-tracing/dockerBuild.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # (c) https://github.com/MontiCore/monticore
 
+cd frontend
+./dockerBuild.sh
+cd -
+
 # cd to root directory /core.
 # this is necessary, as we need to copy the rte directory which would have been out of context otherwise
 cd ../..
--- services/log-tracing/dockerRunLocal.sh
+++ services/log-tracing/dockerRunLocal.sh
@@ -8,10 +8,11 @@
 # On Linux, localhost is used since the host network is used (this is done to allow multicast, see the DDS broker).
 # On iOS, the montithings network is used instead. Therefore, we have to pass the ip of the host machine.
 os="$(uname -s)"
+network=""
 case "${os}" in
-    Linux*)     mqttip=127.0.0.1;;
+    Linux*)     mqttip=127.0.0.1 && network="--net=host";;
     Darwin*)    mqttip=host.docker.internal;;
     *)          echo "unknown os!" && exit 1
 esac
 
-docker run --rm -d --net=host -p 8080:8080 --name logtracer_middleware  registry.git.rwth-aachen.de/monticore/montithings/core/logtracer_middleware --DCPSConfigFile dcpsconfig.ini --mqtt-broker-host ${mqttip} "$@"
+docker run --rm ${network} -p 8080:8080 --name logtracer_middleware  registry.git.rwth-aachen.de/monticore/montithings/core/logtracer_middleware --DCPSConfigFile dcpsconfig.ini --mqtt-broker-host ${mqttip} "$@"
