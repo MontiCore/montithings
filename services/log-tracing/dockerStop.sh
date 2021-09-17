ecd52ec785463a4b0b7d9399e7dc2649192a103a
christian.kirchhof <christian.kirchhof@rwth-aachen.de>
2021-09-17 19:25:59 +0200

    Replace references to RWTH's GitLab registry by Docker Hub

--- services/log-tracing/dockerStop.sh
+++ services/log-tracing/dockerStop.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # (c) https://github.com/MontiCore/monticore
 
-docker stop logtracer_middleware logtracer_frontend
+docker stop logtracer-middleware logtracer-frontend
--- terraform_azure.tf
+++ terraform_azure.tf
@@ -14,16 +14,6 @@ provider "azurerm" {
   features {}
 }
 
-variable "gitlab_name" {
-    type        = string
-    description = "Your RWTH GitLab username"
-}
-
-variable "gitlab_password" {
-    type        = string
-    description = "Your RWTH GitLab password"
-}
-
 variable "rsa_key_location" {
     type        = string
     description = "Path to your private SSH key"
@@ -186,46 +176,12 @@ resource "azurerm_linux_virtual_machine" "montithingsvm" {
         storage_account_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
     }
 
-
-    # These three remote-exec provisioners include
-    # the code that the VM executes to set up MontiThings
-    # It is split up in three parts because Terraform hides
-    # all output if a config uses a sensitive variable.
-    # By splitting this into three parts only the console output
-    # of the first and last part are hidden.
-    # Also the last part needs a separate login to have docker
-    # available after the installation
     provisioner "remote-exec" {
         inline = [
-            "git clone https://${var.gitlab_name}:'${var.gitlab_password}'@git.rwth-aachen.de/monticore/montithings/core.git montithings"
-        ]
-
-        connection {
-          type        = "ssh"
-          user        = azurerm_linux_virtual_machine.montithingsvm.admin_username
-          private_key = file(var.rsa_key_location)
-          host        = azurerm_linux_virtual_machine.montithingsvm.public_ip_address
-        }
-    }
-
-    provisioner "remote-exec" {
-        inline = [
+            "git clone https://github.com/monticore/montithings",
             "cd montithings",
             "sudo cp docs/ascii-banner.txt /etc/motd",
             "./installLinux.sh"
-        ]
-
-        connection {
-            type        = "ssh"
-            user        = azurerm_linux_virtual_machine.montithingsvm.admin_username
-            private_key = file(var.rsa_key_location)
-            host        = azurerm_linux_virtual_machine.montithingsvm.public_ip_address
-        }
-    }
-
-    provisioner "remote-exec" {
-        inline = [
-            "echo '${var.gitlab_password}' | docker login registry.git.rwth-aachen.de --username ${var.gitlab_name} --password-stdin"
         ]
 
         connection {
