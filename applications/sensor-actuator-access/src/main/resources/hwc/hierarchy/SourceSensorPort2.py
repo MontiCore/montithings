563d4561d00e2b57eef55cf01686b05d86c0ed20
Jörg Christian Kirchhof <christian.kirchhof@rwth-aachen.de>
2021-10-19 17:07:14 +0200

    Count up values in sensor examples

--- applications/sensor-actuator-access/src/main/resources/hwc/hierarchy/SourceSensorPort2.py
+++ applications/sensor-actuator-access/src/main/resources/hwc/hierarchy/SourceSensorPort2.py
@@ -6,8 +6,9 @@ import time
 if __name__ == '__main__':
     # Set up MontiThings port connection
     mtc = MTC("second-source-sensor-topic", None, parse_cmd_args=True)
-
+    i = 0
     while True:
         # Send a message to MontiThings (sensor port)
-        mtc.send(32)
+        mtc.send(i)
+        i = i + 1
         time.sleep(2)
