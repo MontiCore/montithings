82071ec04959feb79556250ab2dfb31bd1114a3b
Sebastian Grabowski <sebastian@grabel.de>
2022-06-06 14:56:10 +0200

    tests: Give ListWrapperTest its own directory and cleanup dirs recursively

--- generators/cd2proto/src/test/java/montithings/generator/cd2proto/ListWrapperTest.java
+++ generators/cd2proto/src/test/java/montithings/generator/cd2proto/ListWrapperTest.java
@@ -1,31 +1,51 @@
 package montithings.generator.cd2proto;
 
-import de.se_rwth.commons.logging.Log;
 import montithings.generator.cd2proto.helper.ProtobufRunner;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.io.File;
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public class ListWrapperTest {
 
+    final static Path outDir = Paths.get("target/out/ListWrapperTest");
+
+    private static void deleteRecursive(Path path) throws IOException {
+        if (Files.isDirectory(path)) {
+            try (DirectoryStream<Path> paths = Files.newDirectoryStream(path)) {
+                for (Path p : paths) {
+                    deleteRecursive(p);
+                }
+            }
+        }
+        Files.delete(path);
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws IOException {
+        if (Files.exists(outDir)) {
+            try (DirectoryStream<Path> paths = Files.newDirectoryStream(outDir)) {
+                for (Path p: paths) {
+                    deleteRecursive(p);
+                }
+            }
+            try (Stream<Path> paths = Files.list(outDir)) {
+                if (paths.findAny().isPresent()) {
+                    throw new IllegalStateException("Output directory is not empty");
+                }
+            }
+        } else {
+            Files.createDirectory(outDir);
+        }
+    }
+
     @Test
     public void run() throws Exception {
-        Path outDir = Paths.get("target/out/");
-        Files.list(outDir).forEach(f -> {
-            try {
-                Files.delete(f);
-            } catch(IOException ex) {
-                Log.error("Failed to clean up output directory: " + ex.getMessage(), ex);
-            }
-        });
-        if(Files.list(outDir).findAny().isPresent()) {
-            throw new IllegalStateException("Output directory is not empty");
-        }
-
         Path modelPath = Paths.get("src/test/resources/classdiagrams");
         String modelName = "ListWrapper";
         ProtoGenerator generator = new ProtoGenerator(outDir, modelPath, modelName);
@@ -35,9 +55,12 @@ public class ListWrapperTest {
         ProtobufRunner pr = new ProtobufRunner();
         pr.setTargetLang(ProtobufRunner.TargetLang.JAVA);
         pr.setOutDir(outDir);
-        for(File f : outDir.toFile().listFiles()) {
-            if(!f.getName().endsWith(".proto")) continue;
-            pr.addInputFiles(f.toPath());
+        try (DirectoryStream<Path> paths = Files.newDirectoryStream(outDir)) {
+            for (Path p: paths) {
+                if (p.getFileName().toString().endsWith(".proto")) {
+                    pr.addInputFiles(p);
+                }
+            }
         }
         pr.start();
     }
