081fd0a3356ed67521e9f93e6d1af41a1bea63cd
loeschzwerg <24317401+loeschzwerg@users.noreply.github.com>
2022-07-11 23:44:27 +0200

    merge

--- applications/face-id-door-opener/src/main/resources/hwc/unlock/FaceIDImpl.py
+++ applications/face-id-door-opener/src/main/resources/hwc/unlock/FaceIDImpl.py
@@ -2,11 +2,19 @@ from random import choice
 
 from FaceIDImplTOP import FaceIDImplTOP, FaceIDInput, FaceIDResult
 
+
 class FaceIDImpl(FaceIDImplTOP):
 
+    personDB = {
+        1: "Sebastian",
+        2: "Andre",
+        3: "Danyls",
+        4: "Merlin",
+        5: "Tim"
+    }
     def getInitialValues(self) -> FaceIDResult:
         return FaceIDResult()
 
     def compute(self, _input: FaceIDInput) -> FaceIDResult:
-        result = _input.payload in [ "Sebastian", "Andre", "Tim"]
+        result = self.personDB[_input.payload.personId] in [ "Sebastian", "Andre", "Tim"]
         return FaceIDResult(result)
--- applications/face-id-door-opener/src/main/resources/models/unlock/Camera.mt
+++ applications/face-id-door-opener/src/main/resources/models/unlock/Camera.mt
@@ -1,7 +1,14 @@
 package unlock;
 
 import FaceUnlock.*;
-
 component Camera {
     port out Image.Image image;
+
+    int click = 0;
+    every 1s {
+      image = :Image.Image{
+        personId = click % 5;
+      };
+      click++;
+    }
 }
