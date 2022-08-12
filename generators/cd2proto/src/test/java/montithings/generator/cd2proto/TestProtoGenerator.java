615ae513b87967fea2978baac53cd158c87f7a02
Sebastian Grabowski <sebastian.grabowski@rwth-aachen.de>
2022-08-12 13:26:09 +0200

    Throw a sane exception on parser error
    A runtime error with the message "Whoot?" is obviously not appropriate.

--- generators/cd2proto/src/test/java/montithings/generator/cd2proto/TestProtoGenerator.java
+++ generators/cd2proto/src/test/java/montithings/generator/cd2proto/TestProtoGenerator.java
@@ -1,17 +1,28 @@
 package montithings.generator.cd2proto;
 
+import de.se_rwth.commons.logging.Log;
 import montithings.generator.cd2proto.helper.ProtobufRunner;
+import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assume.assumeTrue;
 
 import java.io.IOException;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Set;
 
+import static org.junit.Assume.assumeTrue;
+
 public class TestProtoGenerator {
+
+  @BeforeClass
+  public static void prepareClass() {
+    Log.enableFailQuick(false);
+  }
+
   @Test
-  public void test() throws IOException {
+  public void test() throws IOException, ParseException {
     Path outDir = Paths.get("target/out/Domain");
     Path modelPath = Paths.get("src/test/resources/models");
     String modelName = "domain.Domain";
@@ -24,5 +35,16 @@ public class TestProtoGenerator {
     pr.setOutDir(outDir);
     protoFiles.forEach(pr::addInputFiles);
     pr.start();
+  }
+
+  @Test
+  public void GIVEN_a_false_model_WHEN_generating_THEN_generator_throws_exception() {
+    Path outDir = Paths.get("target/out/Domain");
+    Path modelPath = Paths.get("src/test/resources/models");
+    ProtoGenerator generator = new ProtoGenerator(outDir, modelPath, "nomodel");
+    Assert.assertThrows(NoSuchFileException.class, generator::generate);
+
+    generator = new ProtoGenerator(outDir, modelPath, "domain.BrokenModel");
+    Assert.assertThrows(ParseException.class, generator::generate);
   }
 }
