0302c419bcb882d1fe1e2bc72ed02c029f595f26
Julius Gummersbach <gummersbach@se-rwth.de>
2022-09-05 15:04:06 +0200

    elevate powershell to admin

--- installWindows.ps1
+++ installWindows.ps1
@@ -1,3 +1,41 @@
+# (c) https://github.com/MontiCore/monticore
+
+# first, make sure powershell is running with admin priviliges
+
+#### START ELEVATE TO ADMIN #####
+param(
+    [Parameter(Mandatory=$false)]
+    [switch]$shouldAssumeToBeElevated,
+
+    [Parameter(Mandatory=$false)]
+    [String]$workingDirOverride
+)
+
+# If parameter is not set, we are propably in non-admin execution. We set it to the current working directory so that
+# the working directory of the elevated execution of this script is the current working directory
+if(-not($PSBoundParameters.ContainsKey('workingDirOverride'))) {
+    $workingDirOverride = (Get-Location).Path
+}
+
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)  {
+    if ($shouldAssumeToBeElevated) {
+        Write-Output "Elevating did not work :("
+    } else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -shouldAssumeToBeElevated -workingDirOverride "{1}"' -f ($myinvocation.MyCommand.Definition, "$workingDirOverride"))
+    }
+    exit
+}
+
+Set-Location "$workingDirOverride"
+##### END ELEVATE TO ADMIN #####
+
+# the shell now runs in admin mode
+
 # Stop on first error
 $ErrorActionPreference = "Stop"
 
