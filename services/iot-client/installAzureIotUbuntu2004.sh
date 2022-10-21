41f57a23edcd87e9c3e2cf9f3bcb41b62a3b5aeb
Julius Gummersbach <gummersbach@se-rwth.de>
2022-10-21 13:02:21 +0200

    set up sensoractuatormanager.py as a service

--- services/iot-client/installAzureIotUbuntu2004.sh
+++ services/iot-client/installAzureIotUbuntu2004.sh
@@ -13,6 +13,28 @@ sudo bash -c "echo \"{
       \\\"max-file\\\": \\\"3\\\"
   }
 }\" > /etc/docker/daemon.json"
+
+
+# set up sensoractuatormanager.py as a service
+sudo bash -c "echo \"[Unit]
+Description=sensor actuator manager for montiThings
+After=mosquitto.service
+StartLimitIntervalSec=0
+[Service]
+ExecStart=/usr/bin/python3 $PYTHON_SCRIPTS_PATH/sensoractuatormanager.py --brokerPort $BROKER_PORT
+Type=simple
+Restart=always
+RestartSec=1
+[Install]
+WantedBy=multi-user.target
+\" > /etc/systemd/system/sensoractuatormanager.service"
+
+sudo systemctl daemon-reload
+sudo systemctl start sensoractuatormanager
+sudo systemctl enable sensoractuatormanager
+
+
+# install and configure azure iot edge
 sudo apt-get -y update
 sudo apt-get -y install aziot-edge defender-iot-micro-agent-edge
 sudo iotedge config mp --connection-string "$CONNECTION_STRING"
