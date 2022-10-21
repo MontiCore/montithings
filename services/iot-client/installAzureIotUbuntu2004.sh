8e20202b9de968b35f8134b5943bd5ad07e4bded
Julius Gummersbach <gummersbach@se-rwth.de>
2022-10-21 13:03:14 +0200

    update installAzureIotUbuntu2004.sh

--- services/iot-client/installAzureIotUbuntu2004.sh
+++ services/iot-client/installAzureIotUbuntu2004.sh
@@ -1,11 +1,25 @@
-CONNECTION_STRING='PASTE_CONNECTION_STRING_HERE'
+# configure your install here
+CONNECTION_STRING='PASTE_DEVICE_CONNECTION_STRING_HERE'
+BROKER_PORT='4230'
 
 wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 sudo apt-get update
 sudo apt-get -y install moby-engine
-sudo bash -c "echo \"{ 
+
+# install and configure mosquitto
+sudo apt-get -y install mosquitto
+sudo mkdir /etc/montithings
+sudo bash -c "echo \"
+persistence true
+persistence_location /var/lib/mosquitto/
+listener 4230 0.0.0.0
+allow_anonymous true
+\" > /etc/mosquitto/mosquitto.conf"
+sudo service mosquitto restart
+
+sudo bash -c "echo \"{
   \\\"log-driver\\\": \\\"local\\\",
   \\\"dns\\\": [\\\"1.1.1.1\\\"],
   \\\"log-opts\\\": {
@@ -39,5 +53,3 @@ sudo apt-get -y update
 sudo apt-get -y install aziot-edge defender-iot-micro-agent-edge
 sudo iotedge config mp --connection-string "$CONNECTION_STRING"
 sudo iotedge config apply -c '/etc/aziot/config.toml'
-
-
