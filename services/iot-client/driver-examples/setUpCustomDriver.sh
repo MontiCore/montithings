6cb9dacabe5ad9d3bb14fdab8cde3e45026eddfa
Julius Gummersbach <gummersbach@se-rwth.de>
2022-10-27 16:38:21 +0200

    fix setUpCustomDriver.sh

--- services/iot-client/driver-examples/setUpCustomDriver.sh
+++ services/iot-client/driver-examples/setUpCustomDriver.sh
@@ -1,10 +1,16 @@
 # (c) https://github.com/MontiCore/monticore
 
 ###################### configuration ##########################
-DRIVER_NAME="<enter name>" # this is what the service that runs your driver will be called
+DRIVER_NAME="<name>" # this is what the service that runs your driver will be called
 PATH_TO_PYTHON_SCRIPT="<enter path>" # put the path to the python script you want to be run by the service
 LOG_DIRECTORY="<enter path>"
 ###############################################################
+
+if [[ $DRIVER_NAME == *" "* ]]; then
+  echo "ERROR: Invalid driver name \"§DRIVER_NAME\""
+  echo "driver name must not contain spaces"
+  exit
+fi
 
 sudo mkdir "$LOG_DIRECTORY"
 sudo bash -c "echo \"[Unit]
@@ -12,7 +18,7 @@ Description=$DRIVER_NAME
 After=mosquitto.service
 StartLimitIntervalSec=0
 [Service]
-ExecStart=/usr/bin/python3 -u $PATH_TO_PYTHON_SCRIPT
+ExecStart=/usr/bin/python3 -u $PATH_TO_PYTHON_SCRIPT --brokerPort 4230
 Type=simple
 StandardOutput=file:$LOG_DIRECTORY/${DRIVER_NAME}_standard.log
 StandardError=file:$LOG_DIRECTORY/${DRIVER_NAME}_error.log
