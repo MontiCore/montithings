ff7745a0e0dfb5d4d1e74f57f71df8b6565c32d7
lazyseals <daniel@bernys-cook.de>
2022-11-15 08:21:12 +0100

    Add terraformInfo field to deployinfo json

--- generators/montithings2cpp/src/main/java/montithings/generator/steps/generate/GenerateDeployInfo.java
+++ generators/montithings2cpp/src/main/java/montithings/generator/steps/generate/GenerateDeployInfo.java
@@ -7,13 +7,23 @@ import montithings.generator.data.GeneratorToolState;
 import montithings.generator.helper.ComponentHelper;
 import montithings.generator.steps.GeneratorStep;
 import org.apache.commons.lang3.tuple.Pair;
+import montiarc.util.Modelfinder;
+import de.se_rwth.commons.logging.Log;
 
 import javax.json.*;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Base64;
+import java.util.List;
+
+import static montithings.generator.MontiThingsGeneratorTool.TOOL_NAME;
 
 public class GenerateDeployInfo extends GeneratorStep {
+  public static final String TF_EXTENSION = "tf";
 
-  @Override public void action(GeneratorToolState state) {
+  @Override
+  public void action(GeneratorToolState state) {
     JsonObjectBuilder jsonBase = Json.createObjectBuilder();
 
     // Collect executable instances.
@@ -33,12 +43,14 @@ public class GenerateDeployInfo extends GeneratorStep {
       for (String req : ComponentHelper.getRequirements(comp, state.getConfig())) {
         if (req.startsWith("ocl:")) {
           jsonInstance.add("hardwareRequirements", req.substring(4));
-        }
-        else {
+        } else {
           jreqs.add(req);
         }
       }
       jsonInstance.add("requirements", jreqs.build());
+
+      JsonArrayBuilder terraformInfo = generateTerraformInfo(state);
+      jsonInstance.add("terraformInfo", terraformInfo);
 
       jsonInstances.add(jsonInstance);
     }
@@ -48,6 +60,38 @@ public class GenerateDeployInfo extends GeneratorStep {
     String jsonString = jsonBase.build().toString();
     File jsonFile = new File(state.getTarget(), "deployment-info.json");
     FileReaderWriter.storeInFile(jsonFile.getAbsoluteFile().toPath(), jsonString);
+  }
+
+  private JsonArrayBuilder generateTerraformInfo(GeneratorToolState state) {
+    List<String> foundModels = Modelfinder.getModelsInModelPath(state.getModelPath(), TF_EXTENSION);
+
+    Log.info("Generating Terraform Base64 String for " + foundModels.size() + " models...", TOOL_NAME);
+
+    JsonArrayBuilder terraformInfos = Json.createArrayBuilder();
+
+    for (String model : foundModels) {
+      Log.info("Generate TF Info: " + model, TOOL_NAME);
+
+      try {
+        JsonObjectBuilder terraformInfo = Json.createObjectBuilder();
+        String modelName = model + "." + TF_EXTENSION;
+        String modelPath = state.getModelPath().getAbsolutePath() + "/" + modelName;
+        String encodedTf = fileToBase64Str(modelPath);
+        terraformInfo.add("filename", modelName);
+        terraformInfo.add("filecontent", encodedTf);
+        terraformInfos.add(terraformInfo);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+
+    return terraformInfos;
+  }
+
+  private String fileToBase64Str(String fileName) throws IOException {
+    File file = new File(fileName);
+    byte[] fileContent = Files.readAllBytes(file.toPath());
+    return Base64.getEncoder().encodeToString(fileContent);
   }
 
 }
