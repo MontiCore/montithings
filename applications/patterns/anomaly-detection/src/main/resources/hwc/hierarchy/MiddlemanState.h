1b6702599bb3ab0dbaab3cef9091dee5e51d74a0
lazyseals <daniel@bernys-cook.de>
2023-01-31 08:16:25 +0100

    Add missing state

---
+++ applications/patterns/anomaly-detection/src/main/resources/hwc/hierarchy/MiddlemanState.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "MiddlemanStateTOP.h"
+
+namespace montithings
+{
+  namespace hierarchy
+  {
+    class MiddlemanState : public MiddlemanStateTOP
+    {
+
+    protected:
+      int runningIndex = 0;
+
+    public:
+      using MiddlemanStateTOP::MiddlemanStateTOP;
+      int getRunningIndex();
+      void postSetRunningIndex(int idx);
+    };
+  } // namespace hierarchy
+} // namespace montithings
