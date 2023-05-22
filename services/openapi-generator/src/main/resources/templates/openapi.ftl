52cfd405e140614d599ee1728f876eecb24ed203
Anno Kleiss <anno.kleiss@rwth-aachen.de>
2023-05-22 13:59:49 +0200

    add service to generate MontiThings interface components from OpenAPI specifications

---
+++ services/openapi-generator/src/main/resources/templates/openapi.ftl
@@ -0,0 +1,10 @@
+<#-- (c) https://github.com/MontiCore/monticore -->
+<#assign PrettyPrinter = tc.instantiate("montithings.services.openapi_generator.openapi.generator.OpenAPIToMTConverter")>
+interface component ${PrettyPrinter.getComponentTypeName(ast)} {
+<#list PrettyPrinter.getIncomingPortNames(ast) as portName>
+  port in String ${portName};
+</#list>
+<#list PrettyPrinter.getStateVariables(ast) as variableName>
+  String ${variableName} = "";
+</#list>
+}
---
+++ services/openapi-generator/src/test/java/montithings/services/openapi_generator/OpenAPIToMTConverterTest.java
@@ -0,0 +1,44 @@
+package montithings.services.openapi_generator;
+
+import de.monticore.lang.json._ast.ASTJSONDocument;
+import de.monticore.lang.json._parser.JSONParser;
+import de.se_rwth.commons.logging.Log;
+import montithings.services.openapi_generator.openapi.generator.OpenAPIToMTConverter;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class OpenAPIToMTConverterTest {
+  @Test
+  public void testParseOpenAPIDocument() throws IOException {
+    Path model = Paths.get("src/test/resources/openapi/SmartDoor.txt");
+    JSONParser parser = new JSONParser();
+
+
+    Optional<ASTJSONDocument> document = parser.parse(model.toString());
+    assertFalse(parser.hasErrors());
+    assertTrue(document.isPresent());
+
+  }
+
+  @Test
+  public void testGenerateMTModel() throws IOException {
+    Path model = Paths.get("src/test/resources/openapi/SmartDoor.txt");
+    JSONParser parser = new JSONParser();
+
+
+    Optional<ASTJSONDocument> document = parser.parse(model.toString());
+    assertFalse(parser.hasErrors());
+    assertTrue(document.isPresent());
+
+
+    String test = OpenAPIToMTConverter.generateMTModel(document.get().getJSONValue());
+    Log.info(test, "OUTPUT");
+  }
+}
